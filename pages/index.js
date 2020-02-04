cdff6868257984f31e6ad5a8cf7a2fd4bfa82338
Július Retzer <jul.retzer@gmail.com>
2020-02-04 17:12:11 +0100

    Add photo and links

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
+import styles from "./index.module.css";
+
 export default function Index() {
   return (
-    <div>
-      <p>Hello, this is Július Retzer</p>
+    <div className={styles.container}>
+      <div className={styles.personalInfo}>
+        <div className={styles.faceContainer}>
+          <img src="/face.jpg" className={styles.face} />
+        </div>
+        <h1 className={styles.name}>Július Retzer</h1>
+        <div className={styles.links}>
+          <a
+            target="_blank"
+            rel="noreferrer noopener nofollow"
+            href="https://github.com/wormyy"
+          >
+            Github
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer noopener nofollow"
+            href="https://stackoverflow.com/users/3992613/j%c3%balius-retzer"
+          >
+            StackOverflow
+          </a>
+          <a target="_blank" href="mailto:julius@retzer.dev">
+            Email
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
