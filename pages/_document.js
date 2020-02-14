cd85bda4c18ac4e09fe0c8432ea23905a74d8864
Július Retzer <jul.retzer@gmail.com>
2020-02-14 22:13:09 +0100

    Fix invalid comment

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <!-- Global site tag (gtag.js) - Google Analytics -->
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158622168-1"></script>
           <script>
             window.dataLayer = window.dataLayer || [];
