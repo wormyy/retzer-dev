fc472759f68c14b5d5e3ad5c728d29f1460cb2e3
Július Retzer <jul.retzer@gmail.com>
2020-02-14 22:35:15 +0100

    Page view analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import "./styles.css";
 import App from "next/app";
+import Router from "next/router";
+
+import * as gtag from "../lib/gtag";
+
+Router.events.on("routeChangeComplete", url => gtag.pageview(url));
 
 export default App;
