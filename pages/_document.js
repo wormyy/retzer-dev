da5344df5f9d96eac77a4cf28e4bb62983dd91ca
Július Retzer <jul.retzer@gmail.com>
2020-02-14 22:30:42 +0100

    Google Analytics next try

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+import { GA_TRACKING_ID } from "../lib/gtag";
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html>
         <Head>
           <script
             async
-            src="https://www.googletagmanager.com/gtag/js?id=UA-158622168-1"
-          ></script>
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
           <script
             dangerouslySetInnerHTML={{
-              __html: ` window.dataLayer = window.dataLayer || [];
+              __html: `
+            window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
-            gtag('config', 'UA-158622168-1');`
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
             }}
-          ></script>
+          />
 
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="icon" href="/favicon.ico" type="image/x-icon" />
